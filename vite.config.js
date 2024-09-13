e1528969add6b7830a98d52eaa9928ee8cbef5b3
AungZawMyo <aung@recsitedesign.com>
2024-09-13 09:50:54 +0700

    deploy state

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MyPortfolio',
   css: {
     postcss: './postcss.config.js', // Ensure PostCSS config is picked up
   }
